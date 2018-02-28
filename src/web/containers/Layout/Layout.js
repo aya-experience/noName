e8442a7c53ff0be9097964238156bc7598e4be32
Agory <gory.alexandre02@gmail.com>
2018-02-28 18:01:17 +0100

    perf(presentional): Compress bulma.css to bulma.min.css

--- src/web/containers/Layout/Layout.js
+++ src/web/containers/Layout/Layout.js
@@ -8,7 +8,7 @@ const Layout = ({ title, children }) => (
     <Head>
       <title>{title}</title>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <link rel="stylesheet" type="text/css" href="/static/bulma.css" />
+      <link rel="stylesheet" type="text/css" href="/static/bulma.min.css" />
     </Head>
     <Container>{children}</Container>
   </div>
