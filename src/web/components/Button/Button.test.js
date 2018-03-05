70fda6e6e43fd1d828bb60b21719228c37202e76
Agory <gory.alexandre02@gmail.com>
2018-03-05 10:16:38 +0100

    feat(presentational): add Button component
    The new button component allow to use Bulma button

---
+++ src/web/components/Button/Button.test.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Button from './';
+import Icon from '../Icon';
+
+describe('Button', () => {
+  let wrapper;
+  let onClick;
+
+  beforeEach(() => {
+    onClick = jest.fn();
+    wrapper = shallow(<Button onClick={onClick}>Hello world</Button>);
+  });
+
+  it('should render children component', () => {
+    expect(wrapper.text()).toEqual('Hello world');
+  });
+
+  it('should trigger on Click an event', () => {
+    wrapper.simulate('click');
+    expect(onClick).toHaveBeenCalledTimes(1);
+  });
+
+  it('should have a disabled button when the prop disabled is true', () => {
+    wrapper.setProps({ disabled: true });
+    const button = wrapper.find('button');
+    expect(button.prop('disabled')).toEqual(true);
+  });
+
+  it('should have a is-outlined class when the prop outlined is true', () => {
+    wrapper.setProps({ outlined: true });
+    const button = wrapper.find('button.is-outlined');
+    expect(button).toHaveLength(1);
+  });
+
+  it('should have a is-inverted class when the prop inverted is true', () => {
+    wrapper.setProps({ inverted: true });
+    const button = wrapper.find('button.is-inverted');
+    expect(button).toHaveLength(1);
+  });
+
+  it('should have a is-rounded class when the prop rounded is true', () => {
+    wrapper.setProps({ rounded: true });
+    const button = wrapper.find('button.is-rounded');
+    expect(button).toHaveLength(1);
+  });
+
+  it('should have a is-loading class when the prop loading is true', () => {
+    wrapper.setProps({ loading: true });
+    const button = wrapper.find('button.is-loading');
+    expect(button).toHaveLength(1);
+  });
+
+  it('should have a button with a color class when the props color is set', () => {
+    wrapper.setProps({ color: 'primary' });
+    const button = wrapper.find('button.is-primary');
+    expect(button).toHaveLength(1);
+  });
+
+  it('should have a icon component with children prop', () => {
+    wrapper.setProps({ icon: 'ion-home' });
+    const icon = wrapper.find(Icon);
+    expect(icon.prop('children')).toBe('ion-home');
+  });
+
+  it('should have a button with a size class when the props size is set', () => {
+    wrapper.setProps({ size: 'small' });
+    const button = wrapper.find('button.is-small');
+    expect(button).toHaveLength(1);
+  });
+});
---
+++ src/web/components/Button/index.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Icon from '../Icon';
+
+export default function Button({
+  children,
+  onClick,
+  color,
+  disabled,
+  size,
+  outlined,
+  inverted,
+  rounded,
+  loading,
+  icon,
+}) {
+  const classes = `button ${color ? `is-${color}` : ''} ${size ? ` is-${size}` : ''} ${
+    outlined ? 'is-outlined' : ''
+  } ${inverted ? 'is-inverted' : ''} ${rounded ? 'is-rounded' : ''} ${loading ? 'is-loading' : ''}`;
+  const iconComponent = icon ? <Icon>{icon}</Icon> : null;
+  return (
+    <button className={classes} disabled={disabled} onClick={onClick}>
+      {iconComponent}
+      {children}
+    </button>
+  );
+}
+
+Button.propTypes = {
+  children: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.element,
+    PropTypes.arrayOf(PropTypes.element),
+  ]).isRequired,
+  onClick: PropTypes.func.isRequired,
+  color: PropTypes.string,
+  size: PropTypes.string,
+  icon: PropTypes.string,
+  disabled: PropTypes.bool,
+  outlined: PropTypes.bool,
+  inverted: PropTypes.bool,
+  rounded: PropTypes.bool,
+  loading: PropTypes.bool,
+};
+
+Button.defaultProps = {
+  color: '',
+  size: '',
+  icon: '',
+  disabled: false,
+  outlined: false,
+  inverted: false,
+  rounded: false,
+  loading: false,
+};
