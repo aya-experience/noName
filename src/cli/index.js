1fc998edbb54eed0d19c90d27898ada3f63f0952
Agory <gory.alexandre02@gmail.com>
2018-03-05 15:47:57 +0100

    feat(cli): basic cli config
    setup the cli config to run the next.js server

--- src/cli/index.js
+++ src/cli/index.js
@@ -2,5 +2,16 @@
  * CLI goes here
  */
 const program = require('commander');
+const packages = require('../../package');
 
-console.log('Cli goes here');
+program
+  .version(packages.version)
+  .description('An application for debug React-Native App')
+  .option('-d', '--developpment', 'Developpment mode')
+  .parse(process.argv);
+
+if (program.developpment) {
+  process.env.NODE_ENV = 'developpment';
+}
+
+require('../server/httpServer');
