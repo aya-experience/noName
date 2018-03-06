45dc1f52ed54ae81d8287bd5c940fca5efd9a445
GoryAlexandre <gory.alexandre02@gmail.com>
2018-03-06 13:58:16 +0100

    feat(cli): basic cli config (#10)
    * feat(cli): basic cli config
    
    setup the cli config to run the next.js server
    
    * fix(cli): update running method and remove -d arg
    
    * fix(cli): update description
    
    * feat(cli): add option --port
    
    add the choice of the port and use package description
    
    * fix(cli): update the info message for the port option
    
    * feat(cli): script : serve with cli
    
    * fix(cli): update option -d description

--- src/server/httpServer.js
+++ src/server/httpServer.js
@@ -4,15 +4,15 @@ const next = require('next');
 const open = require('opn');
 
 const DEV = process.env.NODE_ENV !== 'production';
-const SERVER_ADRESS = 'http://localhost:3000';
 
-const serverHandler = (err) => {
-  if (err) throw err;
-  open(SERVER_ADRESS);
-  console.log(`> Ready on ${SERVER_ADRESS}`);
-};
+const startServer = async (port) => {
+  const SERVER_ADRESS = `http://localhost:${port}`;
+  const serverHandler = (err) => {
+    if (err) throw err;
+    open(SERVER_ADRESS);
+    console.log(`> Ready on ${SERVER_ADRESS}`);
+  };
 
-const startServer = async () => {
   try {
     const server = express();
     const app = next({ dev: DEV });
@@ -21,11 +21,11 @@ const startServer = async () => {
     await app.prepare();
 
     server.get('*', handle);
-    server.listen(3000, serverHandler);
+    server.listen(port, serverHandler);
   } catch (ex) {
     console.error(ex.stack);
     process.exit(1);
   }
 };
 
-startServer();
+module.exports = startServer;
