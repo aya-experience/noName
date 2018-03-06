2593ef08661fb2471a5a2214105e84cca4d0ef4a
Agory <gory.alexandre02@gmail.com>
2018-03-06 09:35:51 +0100

    fix(cli): update the info message for the port option

--- src/cli/index.js
+++ src/cli/index.js
@@ -8,7 +8,7 @@ const startServer = require('../server/httpServer');
 program
   .version(packageInfo.version)
   .description(packageInfo.description)
-  .option('-p, --port [type]', 'run the app on the port [3000]', 3000)
+  .option('-p, --port [type]', 'run the app on the port [port]', 3000)
   .parse(process.argv);
 
 startServer(program.port);
