31eef0a0a661cf9f18aaad13ea104ba90a5ff15a
Agory <gory.alexandre02@gmail.com>
2018-03-06 16:45:11 +0100

    feat(presentational): add Title component
    Component Title : props children (Text), size (Number). Use element title of bulma.

---
+++ src/web/components/Title/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default function Title({ children, size }) {
+  const classes = `title${` is-${size}`}`;
+  return <h1 className={classes}>{children}</h1>;
+}
+
+Title.propTypes = {
+  children: PropTypes.string.isRequired,
+  size: PropTypes.number,
+};
+
+Title.defaultProps = {
+  size: 3,
+};
