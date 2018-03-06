d5374e8759cabc4f06f64f27a2c1eea51a2fbde7
Agory <gory.alexandre02@gmail.com>
2018-03-06 16:01:37 +0100

    test(presentational): deletion of duplicated test

--- src/web/components/Box/box.test.js
+++ src/web/components/Box/box.test.js
@@ -11,11 +11,7 @@ describe('Box', () => {
     wrapper = shallow(<Container>{content}</Container>);
   });
 
-  it('should render children component', () => {
-    expect(wrapper.children()).toHaveLength(1);
-  });
-
-  it('should render children component with a text', () => {
+  it('should render a children component with a text', () => {
     const children = wrapper.find('div#hello');
     expect(children.text()).toEqual('Hello world');
   });
