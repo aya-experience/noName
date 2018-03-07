87d55ea7abe904b650c4c871328b9c1c794179fb
Agory <gory.alexandre02@gmail.com>
2018-03-07 10:03:56 +0100

    feat(storybook): add action in button stories

--- stories/button.stories.js
+++ stories/button.stories.js
@@ -1,48 +1,95 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
 import Button from '../src/web/components/Button';
 import Icon from '../src/web/components/Icon';
 
 storiesOf('Button', module)
-  .add('basic button', () => <Button>Simple button</Button>)
+  .add('basic button', () => <Button onClick={action('button-click')}>Simple button</Button>)
   .add('with size', () => [
-    <Button size="small">Simple button</Button>,
-    <Button>Simple button</Button>,
-    <Button size="medium">Simple button</Button>,
-    <Button size="large">Simple button</Button>,
+    <Button onClick={action('button-click1')} size="small">
+      Simple button
+    </Button>,
+    <Button onClick={action('button-click2')}>Simple button</Button>,
+    <Button onClick={action('button-click3')} size="medium">
+      Simple button
+    </Button>,
+    <Button onClick={action('button-click4')} size="large">
+      Simple button
+    </Button>,
   ])
   .add('with color', () => [
-    <Button>Simple button</Button>,
-    <Button color="link">Simple button</Button>,
-    <Button color="info">Simple button</Button>,
-    <Button color="success">Simple button</Button>,
-    <Button color="warning">Simple button</Button>,
-    <Button color="danger">Simple button</Button>,
-    <Button color="light">Simple button</Button>,
-    <Button color="white">Simple button</Button>,
-    <Button color="dark">Simple button</Button>,
-    <Button color="black">Simple button</Button>,
-    <Button color="text">Simple button</Button>,
+    <Button onClick={action('button-click1')}>Simple button</Button>,
+    <Button onClick={action('button-click2')} color="link">
+      Simple button
+    </Button>,
+    <Button onClick={action('button-click3')} color="info">
+      Simple button
+    </Button>,
+    <Button onClick={action('button-click4')} color="success">
+      Simple button
+    </Button>,
+    <Button onClick={action('button-click5')} color="warning">
+      Simple button
+    </Button>,
+    <Button onClick={action('button-click6')} color="danger">
+      Simple button
+    </Button>,
+    <Button onClick={action('button-click7')} color="light">
+      Simple button
+    </Button>,
+    <Button onClick={action('button-click8')} color="white">
+      Simple button
+    </Button>,
+    <Button onClick={action('button-click9')} color="dark">
+      Simple button
+    </Button>,
+    <Button onClick={action('button-click10')} color="black">
+      Simple button
+    </Button>,
+    <Button onClick={action('button-click11')} color="text">
+      Simple button
+    </Button>,
   ])
   .add('with icon', () => [
-    <Button>
+    <Button onClick={action('button-click1')}>
       <Icon name="ion-ionic" />Before
     </Button>,
-    <Button>
+    <Button onClick={action('button-click2')}>
       After<Icon name="ion-home" />
     </Button>,
-    <Button>
+    <Button onClick={action('button-click3')}>
       <Icon name="ion-social-android" color="danger" />2 in same time<Icon
         name="ion-social-javascript"
         color="primary"
       />
     </Button>,
-    <Button>
+    <Button onClick={action('button-click4')}>
       <Icon name="ion-social-octocat" color="info" />
     </Button>,
   ])
-  .add('with disabled', () => <Button disabled>Simple button</Button>)
-  .add('with outlined', () => <Button outlined>Simple button</Button>)
-  .add('with inverted', () => <Button inverted>Simple button</Button>)
-  .add('with rounded', () => <Button rounded>Simple button</Button>)
-  .add('with loading', () => <Button loading>Simple button</Button>);
+  .add('with disabled', () => (
+    <Button onClick={action('button-click')} disabled>
+      Simple button
+    </Button>
+  ))
+  .add('with outlined', () => (
+    <Button onClick={action('button-click')} outlined>
+      Simple button
+    </Button>
+  ))
+  .add('with inverted', () => (
+    <Button onClick={action('button-click')} inverted>
+      Simple button
+    </Button>
+  ))
+  .add('with rounded', () => (
+    <Button onClick={action('button-click')} rounded>
+      Simple button
+    </Button>
+  ))
+  .add('with loading', () => (
+    <Button onClick={action('button-click')} loading>
+      Simple button
+    </Button>
+  ));
