9dc7e5d5be315ebf0af73d5def85b6a72ae84d63
GoryAlexandre <gory.alexandre02@gmail.com>
2018-03-07 10:05:18 +0100

    feat(storybook): add icon stories (#13)
    * fix(presentational): add ionicon font lib in static
    
    * feat(storybook): add icon stories
    
    config stories to use ionicon lib
    
    * fix(storybook): remove icon on index page

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,5 +1,6 @@
 import { configure } from '@storybook/react';
-import './../static/bulma.min.css';
+import './../static/css/bulma.min.css';
+import './../static/css/ionicons.min.css';
 
 // automatically import all files ending in *.stories.js
 const req = require.context('../stories', true, /.stories.js$/);
