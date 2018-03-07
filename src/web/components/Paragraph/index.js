cce2335c155847176ecdce44718d4a63105f1cbd
Agory <gory.alexandre02@gmail.com>
2018-03-07 14:47:39 +0100

    fix(presentational): update componsant name

--- src/web/components/Paragraph/index.js
+++ src/web/components/Paragraph/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export default function Paragraph({ children }) {
+export default function Text({ children }) {
   return <p>{children}</p>;
 }
 
-Paragraph.propTypes = {
+Text.propTypes = {
   children: PropTypes.string.isRequired,
 };
