8ed4de48c4ae2e391345e1541402483c0efa994c
GoryAlexandre <gory.alexandre02@gmail.com>
2018-03-07 11:20:13 +0100

    feat(presentational): add Title component (#17)
    * feat(presentational): add Title component
    
    Component Title : props children (Text), size (Number). Use element title of bulma.
    
    * test(presentational): fix a mistake on last test
    
    * test(presentational): fix it description
    
    * fix(improve code quality):

---
+++ src/web/components/Title/Title.test.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Title from './';
+
+describe('Title', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<Title>Hello World</Title>);
+  });
+
+  it('should render a h1 with children as a text', () => {
+    expect(wrapper.find('h1.title').text()).toBe('Hello World');
+  });
+
+  it('should render a h1 with is-3 by default', () => {
+    expect(wrapper.find('h1.title.is-3')).toHaveLength(1);
+  });
+
+  it('should render a h1 with is-1 if size = 1', () => {
+    wrapper.setProps({ size: 1 });
+    expect(wrapper.find('h1.title.is-1')).toHaveLength(1);
+  });
+
+  it('should render a h1 with is-6 if size = 6', () => {
+    wrapper.setProps({ size: 6 });
+    expect(wrapper.find('h1.title.is-6')).toHaveLength(1);
+  });
+});
