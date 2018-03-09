0fc41fac0a8b06553f1c426a9edc545c99f555a4
Agory <gory.alexandre02@gmail.com>
2018-03-09 16:48:25 +0100

    feat(presentational): add Menu component
    Creation of menu function to render menu and recursively submenu (as Menu component)

--- src/web/components/Menu/Menu.test.js
+++ src/web/components/Menu/Menu.test.js
@@ -1,11 +1,52 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import Menu from './';
+import Subtitle from '../Subtitle';
+import List from '../List';
 
 it('Menu', () => {
-  it('should render a title menu', () => {});
-  it('should render a list with menu item', () => {});
-  it('should render a sub menu when a menu item have an another menu', () => {});
-  it('should trigger a click event when a title menu is clicked', () => {});
-  it('should trigger a click event when a sub menu is clicked', () => {});
+  let wrapper;
+  let menuData;
+
+  beforeEach(() => {
+    // 3 level menu
+    menuData = {
+      name: 'Administration',
+      items: [
+        {
+          name: 'User',
+          items: [
+            { name: 'List' },
+            { name: 'Add' },
+            { name: 'Remove' },
+            { name: 'Update' },
+            {
+              name: 'Categories',
+              items: [{ name: 'List' }, { name: 'Add' }, { name: 'Remove' }, { name: 'Update' }],
+            },
+          ],
+        },
+        { name: 'About' },
+      ],
+    };
+    wrapper = shallow(<Menu data={menuData} />);
+  });
+
+  it('should render a title menu', () => {
+    const title = wrapper.find(Subtitle).first();
+    expect(title.prop('chidren')).toBe(menuData.name);
+  });
+
+  it('should render a list with menu item', () => {
+    const list = wrapper.find(List).first();
+    expect(list.prop('data')).toBe(menuData.items);
+  });
+  it('should render a sub menu when a menu item have an another menu', () => {
+    const subMenu = wrapper.find(Menu).first();
+    expect(subMenu.prop('data')).toBe(menuData.items[0]);
+  });
+
+  // TODO later
+  // it('should trigger a click event when a title menu is clicked', () => {});
+  // it('should trigger a click event when a sub menu is clicked', () => {});
 });
