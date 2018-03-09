6eb40aad8947b430a5591fd740eb822ebfb2df8e
Agory <gory.alexandre02@gmail.com>
2018-03-09 17:40:26 +0100

    fix(presentational): Improve test coverage end fix menu renderItem

--- src/web/components/Menu/Menu.test.js
+++ src/web/components/Menu/Menu.test.js
@@ -3,8 +3,9 @@ import { shallow } from 'enzyme';
 import Menu from './';
 import Subtitle from '../Subtitle';
 import List from '../List';
+import Text from '../Text';
 
-it('Menu', () => {
+describe('Menu', () => {
   let wrapper;
   let menuData;
 
@@ -34,16 +35,28 @@ it('Menu', () => {
 
   it('should render a title menu', () => {
     const title = wrapper.find(Subtitle).first();
-    expect(title.prop('chidren')).toBe(menuData.name);
+    expect(title.prop('children')).toBe(menuData.name);
   });
 
   it('should render a list with menu item', () => {
     const list = wrapper.find(List).first();
     expect(list.prop('data')).toBe(menuData.items);
   });
+
   it('should render a sub menu when a menu item have an another menu', () => {
-    const subMenu = wrapper.find(Menu).first();
-    expect(subMenu.prop('data')).toBe(menuData.items[0]);
+    const subMenu = wrapper
+      .find(List)
+      .first()
+      .prop('renderItem')(menuData.items[0]);
+    expect(subMenu.type).toBe(Menu);
+  });
+
+  it("should render a Text component when the item menu doesn't have items", () => {
+    const menuItem = wrapper
+      .find(List)
+      .first()
+      .prop('renderItem')(menuData.items[1]);
+    expect(menuItem.props.children).toBe(menuData.items[1].name);
   });
 
   // TODO later
