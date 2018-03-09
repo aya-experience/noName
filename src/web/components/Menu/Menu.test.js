c70a3c7731c373377b2b33b466bc19d4c84fc5b1
Agory <gory.alexandre02@gmail.com>
2018-03-09 15:51:20 +0100

    test(presentational): Add test structure

---
+++ src/web/components/Menu/Menu.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Menu from './';
+
+it('Menu', () => {
+  it('should render a title menu', () => {});
+  it('should render a list with menu item', () => {});
+  it('should render a sub menu when a menu item have an another menu', () => {});
+  it('should trigger a click event when a title menu is clicked', () => {});
+  it('should trigger a click event when a sub menu is clicked', () => {});
+});
