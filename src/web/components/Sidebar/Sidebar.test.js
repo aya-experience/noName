12b078268127916d8f3c73d21a58e382a0f38824
Agory <gory.alexandre02@gmail.com>
2018-03-12 17:41:59 +0100

    feat(presentational): move style in component file
    improve test

--- src/web/components/Sidebar/Sidebar.test.js
+++ src/web/components/Sidebar/Sidebar.test.js
@@ -16,7 +16,50 @@ describe('Sidebar', () => {
   it('should render children component', () => {
     expect(wrapper.contains(children));
   });
+
   it('should render sidebar content', () => {
     expect(wrapper.contains(children));
+  });
+
+  it('should render a div with #efefef backgorund color by default', () => {
+    const color = wrapper
+      .find('div')
+      .children()
+      .first()
+      .prop('style').backgroundColor;
+
+    expect(color).toBe('#efefef');
+  });
+
+  it('should render a div with #eeeeee backgorund color', () => {
+    wrapper.setProps({ color: '#eeeeee' });
+    const color = wrapper
+      .find('div')
+      .children()
+      .first()
+      .prop('style').backgroundColor;
+
+    expect(color).toBe('#eeeeee');
+  });
+
+  it('should render a div with 160px width by default', () => {
+    const color = wrapper
+      .find('div')
+      .children()
+      .first()
+      .prop('style').width;
+
+    expect(color).toBe('160px');
+  });
+
+  it('should render a div with 200px width', () => {
+    wrapper.setProps({ width: '200px' });
+    const color = wrapper
+      .find('div')
+      .children()
+      .first()
+      .prop('style').width;
+
+    expect(color).toBe('200px');
   });
 });
--- src/web/components/Sidebar/index.js
+++ src/web/components/Sidebar/index.js
@@ -1,13 +1,34 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import computeStyle from './sidebar.style';
 
-const Sidebar = (props) => {
-  const { children, content } = props;
-  const style = computeStyle(props);
+/**
+ * Compute style based on props
+ * @param color The line color
+ * @param width The line width
+ */
+const computeStyle = (color, width) => ({
+  drawer: {
+    height: '100%',
+    width,
+    position: 'fixed',
+    zIndex: 1,
+    top: 0,
+    left: 0,
+    backgroundColor: color,
+    overflowX: 'hidden',
+  },
+  container: {
+    paddingLeft: width,
+  },
+});
+
+const Sidebar = ({
+  children, content, color, width,
+}) => {
+  const styles = computeStyle(color, width);
   return (
-    <div style={style.container}>
-      <div style={style.drawer}>{content}</div>
+    <div style={styles.container}>
+      <div style={styles.drawer}>{content}</div>
       {children}
     </div>
   );
@@ -16,8 +37,13 @@ const Sidebar = (props) => {
 Sidebar.propTypes = {
   children: PropTypes.element.isRequired,
   content: PropTypes.element.isRequired,
+  color: PropTypes.string,
+  width: PropTypes.string,
 };
 
-Sidebar.defaultProps = {};
+Sidebar.defaultProps = {
+  color: '#efefef',
+  width: '160px',
+};
 
 export default Sidebar;
