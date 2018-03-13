bb65239ccb300d0e4947214e2df8df6d5995d7bc
Agory <gory.alexandre02@gmail.com>
2018-03-13 10:51:18 +0100

    feat(presentational): add size hoc

---
+++ src/web/hoc/Size/Size.test.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import connect from './';
+
+describe('Size', () => {
+  let wrapper;
+  let DivComponent;
+
+  beforeEach(() => {
+    DivComponent = props => <div {...props}>Hello World</div>;
+    const SizeDiv = connect(DivComponent);
+    wrapper = shallow(<SizeDiv />);
+  });
+
+  it('should render the connected component without class by default', () => {
+    expect(wrapper.find(DivComponent).prop('className')).toBe('');
+  });
+
+  it('should render the connected component with is-large classes', () => {
+    wrapper.setProps({ size: 'large' });
+    expect(wrapper.find(DivComponent).prop('className')).toBe('is-large');
+  });
+
+  it('should transfert className props to the connected component', () => {
+    wrapper.setProps({ className: 'hello' });
+    expect(wrapper.find(DivComponent).prop('className')).toBe('hello');
+  });
+});
