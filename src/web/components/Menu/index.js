3d32d27b2bc2e6520288cc34583e3193da9f9f86
Agory <gory.alexandre02@gmail.com>
2018-03-13 17:40:46 +0100

    fix(presentational): fix check items array

--- src/web/components/Menu/index.js
+++ src/web/components/Menu/index.js
@@ -1,31 +1,38 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Subtitle from '../Subtitle';
 import Text from '../Text';
 import List from '../List';
+import Item from '../Item';
 
 const renderItem = (item) => {
-  if (Object.prototype.hasOwnProperty.call(item, 'items') && item.items.length > 0) {
-    return <Menu data={item} />;
+  if (item.items && item.items.length) {
+    return (
+      <Item key={item.name}>
+        <Menu data={item} />
+      </Item>
+    );
   }
-  return <Text>{item.name}</Text>;
+  return (
+    <Item key={item.name}>
+      <Text>{item.name}</Text>
+    </Item>
+  );
 };
 
 export default function Menu({ data }) {
   return (
     <nav>
-      <Subtitle>{data.name}</Subtitle>
-      <List data={data.items} renderItem={renderItem} />
+      <Text className="menu-label">{data.name}</Text>
+      <List className="menu-list" data={data.items} renderItem={renderItem} />
     </nav>
   );
 }
 
-// TODO ask help for 'menuType' was used before it was defined. (no-use-before-define)
 const menuType = {
   name: PropTypes.string.isRequired,
   items: PropTypes.arrayOf(PropTypes.oneOfType([
     PropTypes.shape({ name: PropTypes.string.isRequired }),
-    PropTypes.shape(menuType),
+    PropTypes.shape({}),
   ])).isRequired,
 };
 
