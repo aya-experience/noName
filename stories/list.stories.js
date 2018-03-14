232502fed6df2c6a00026efd1a10d458764dfdff
Agory <gory.alexandre02@gmail.com>
2018-03-14 09:53:40 +0100

    fix(presentational): update list stories

--- stories/list.stories.js
+++ stories/list.stories.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import List from './../src/web/components/List';
+import Item from './../src/web/components/Item';
 
 const data = [
   { id: 'hashID1', name: 'hamtaro' },
@@ -8,8 +9,6 @@ const data = [
   { id: 'hashID3', name: 'aya' },
 ];
 
-const renderItem = item => <div>{item.name}</div>;
+const renderItem = item => <Item key={item.id}>{item.name}</Item>;
 
-storiesOf('List', module)
-  .add('Basic list', () => <List data={data} renderItem={renderItem} />)
-  .add('Using personalized Key', () => <List data={data} renderItem={renderItem} keyName="id" />);
+storiesOf('List', module).add('Basic list', () => <List data={data} renderItem={renderItem} />);
