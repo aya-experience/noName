a9023e7a9dd3ab1f362e939cc25fba84d288dcfd
Agory <gory.alexandre02@gmail.com>
2018-03-14 13:48:18 +0100

    test(fix text mistake):

--- src/web/components/Sidebar/Sidebar.test.js
+++ src/web/components/Sidebar/Sidebar.test.js
@@ -21,7 +21,7 @@ describe('Sidebar', () => {
     expect(wrapper.contains(children));
   });
 
-  it('should render a div with #efefef backgorund color by default', () => {
+  it('should render a div with #efefef background color by default', () => {
     const color = wrapper
       .find('div')
       .children()
@@ -31,7 +31,7 @@ describe('Sidebar', () => {
     expect(color).toBe('#efefef');
   });
 
-  it('should render a div with #eeeeee backgorund color', () => {
+  it('should render a div with #eeeeee background color', () => {
     wrapper.setProps({ color: '#eeeeee' });
     const color = wrapper
       .find('div')
@@ -43,23 +43,23 @@ describe('Sidebar', () => {
   });
 
   it('should render a div with 160px width by default', () => {
-    const color = wrapper
+    const width = wrapper
       .find('div')
       .children()
       .first()
       .prop('style').width;
 
-    expect(color).toBe('160px');
+    expect(width).toBe('160px');
   });
 
   it('should render a div with 200px width', () => {
     wrapper.setProps({ width: '200px' });
-    const color = wrapper
+    const width = wrapper
       .find('div')
       .children()
       .first()
       .prop('style').width;
 
-    expect(color).toBe('200px');
+    expect(width).toBe('200px');
   });
 });
