775b0ea4c49c61deed3d6ac58562e8003c9d88f3
GoryAlexandre <gory.alexandre02@gmail.com>
2018-03-19 16:11:11 +0100

    feat(presentational): add size hoc  (#30)
    * feat(presentational): add size hoc
    
    * refactor(presentational): rename size hoc : connect to sizable

---
+++ src/web/hoc/Size/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default function sizable(Component) {
+  const Size = ({ className, size, ...props }) => {
+    const classes = className + (size && ` is-${size}`);
+    return <Component {...props} className={classes.trim()} />;
+  };
+
+  Size.propTypes = {
+    size: PropTypes.oneOf(['', 'small', 'medium', 'large']),
+    className: PropTypes.string,
+  };
+
+  Size.defaultProps = {
+    size: '',
+    className: '',
+  };
+
+  return Size;
+}
