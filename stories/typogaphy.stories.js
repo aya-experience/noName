b0958dc9cb36c7c4eb1ec298511e2fc105ed5d07
Agory <gory.alexandre02@gmail.com>
2018-03-23 14:15:58 +0100

    feat(storybook): update storybook with hoc typography functionality

--- stories/typogaphy.stories.js
+++ stories/typogaphy.stories.js
@@ -23,4 +23,46 @@ storiesOf('Typography', module)
     <Subtitle level={4}>Size 4</Subtitle>,
     <Subtitle level={5}>Size 5</Subtitle>,
   ])
-  .add('Text', () => [<Text>Hello world</Text>]);
+  .add('Text', () => [<Text>Hello world</Text>])
+  .add('with Color', () => [
+    <Text color="">Default</Text>,
+    <Text color="black">Black</Text>,
+    <Text color="light">Light</Text>,
+    <Text color="dark">Dark</Text>,
+    <Text color="primary">Primary</Text>,
+    <Text color="info">Info</Text>,
+    <Text color="link">Link</Text>,
+    <Text color="success">Success</Text>,
+    <Text color="warning">Warning</Text>,
+    <Text color="danger">Danger</Text>,
+    <Text color="black-bis">Black bis</Text>,
+    <Text color="black-ter">Black ter</Text>,
+    <Text color="grey-darker">Grey darker</Text>,
+    <Text color="grey-dark">Grey dark</Text>,
+    <Text color="grey">Grey</Text>,
+    <Text color="grey-light">Grey light</Text>,
+    <Text color="grey-lighter">Grey lighter</Text>,
+    <Text color="white-ter">white-ter</Text>,
+    <Text color="white-bis">white-bis</Text>,
+  ])
+  .add('with Align', () => [
+    <Text align="">Default</Text>,
+    <Text align="centered">Centered</Text>,
+    <Text align="justified">Justified</Text>,
+    <Text align="left">Left</Text>,
+    <Text align="right">Right</Text>,
+  ])
+  .add('with Transformation', () => [
+    <Text transformation="">Default</Text>,
+    <Text transformation="capitalized">Capitalized</Text>,
+    <Text transformation="lowercase">Lowercase</Text>,
+    <Text transformation="uppercase">Uppercase</Text>,
+    <Text transformation="italic">Italic</Text>,
+  ])
+  .add('with Weight', () => [
+    <Text color="">Default</Text>,
+    <Text color="light">Light</Text>,
+    <Text color="normal">Normal</Text>,
+    <Text color="semibold">Semibold</Text>,
+    <Text color="bold">Bold</Text>,
+  ]);
