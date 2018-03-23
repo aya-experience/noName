dc75b4105627e3fba0bfd1b9419020ffbbfd76e2
Agory <gory.alexandre02@gmail.com>
2018-03-23 11:51:05 +0100

    fix(presentational): fix some mistake

--- src/web/components/Pagination/Pagination.test.js
+++ src/web/components/Pagination/Pagination.test.js
@@ -17,12 +17,12 @@ describe('Pagination', () => {
     expect(button.prop('children')).toBe('1');
   });
 
-  it('should render the last page', () => {
+  it('should render the last page number', () => {
     const button = wrapper.find(Button).last();
     expect(button.prop('children')).toBe('100');
   });
 
-  it('should render the current page', () => {
+  it('should render the current page number', () => {
     const button = wrapper.find(Button).at(4);
     expect(button.prop('children')).toBe('50');
   });
@@ -30,6 +30,11 @@ describe('Pagination', () => {
   it('should render the current page  number with is-current class', () => {
     const button = wrapper.find(Button).at(4);
     expect(button.hasClass('is-current')).toBeTruthy();
+  });
+
+  it('should render the current page  number with the disabled prop', () => {
+    const button = wrapper.find(Button).at(4);
+    expect(button.prop('disabled')).toBeTruthy();
   });
 
   it('should render the previous page number', () => {
@@ -66,12 +71,12 @@ describe('Pagination', () => {
     expect(buttons).toHaveLength(6);
   });
 
-  it('should not render the previous button when the first page is selected', () => {
+  it('should render a disabled previous button when the first page is selected', () => {
     wrapper.setProps({ currentPage: 1 });
     expect(wrapper.find('.pagination-previous').prop('disabled')).toBeTruthy();
   });
 
-  it('should not render the next button when the last page is selected', () => {
+  it('should not render a disabled next button when the last page is selected', () => {
     wrapper.setProps({ currentPage: 100 });
     expect(wrapper.find('.pagination-next').prop('disabled')).toBeTruthy();
   });
@@ -88,31 +93,31 @@ describe('Pagination', () => {
     expect(onChange).toBeCalledWith(51);
   });
 
-  it('should trigger onChange event with a page number when a user click on a first page button', () => {
+  it('should trigger onChange event with a page number when a user click on the first page button', () => {
     const button = wrapper.find(Button).at(2);
     button.simulate('click');
     expect(onChange).toBeCalledWith(1);
   });
 
-  it('should trigger onChange event with a page number when a user click on a last page button', () => {
+  it('should trigger onChange event with a page number when a user click on the last page button', () => {
     const button = wrapper.find(Button).last();
     button.simulate('click');
     expect(onChange).toBeCalledWith(100);
   });
 
-  it('should trigger onChange event with a page number when a user click on a page before current page', () => {
+  it('should trigger onChange event with a page number when a user click on the number before the current number', () => {
     const button = wrapper.find(Button).at(3);
     button.simulate('click');
     expect(onChange).toBeCalledWith(49);
   });
 
-  it('should trigger onChange event with a page number when a user click on a page after current page', () => {
+  it('should trigger onChange event with a page number when a user click on the number after current number', () => {
     const button = wrapper.find(Button).at(5);
     button.simulate('click');
     expect(onChange).toBeCalledWith(51);
   });
 
-  it('should not trigger when the user click on the current page', () => {
+  it('should not trigger onChange event when the user click on the current page', () => {
     const button = wrapper.find(Button).at(4);
     button.simulate('click');
     expect(onChange).not.toBeCalled();
