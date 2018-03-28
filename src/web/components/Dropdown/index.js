c9194521602b21fc12db6d7474d855c8e365e5c7
Agory <gory.alexandre02@gmail.com>
2018-03-28 17:12:35 +0200

    feat(presentational): update dropdown

--- src/web/components/Dropdown/index.js
+++ src/web/components/Dropdown/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Button from '../Button';
+import COLOR from '../../constant';
 
 class Dropdown extends Component {
   constructor(props) {
@@ -16,18 +17,21 @@ class Dropdown extends Component {
 
   render() {
     const { open } = this.state;
-    const content = open && (
+    const { content, color, children } = this.props;
+    const menu = open && (
       <div className="dropdown-menu" id="dropdown-menu4" role="menu">
-        <div className="dropdown-content">{this.props.content}</div>
+        <div className="dropdown-content">{content}</div>
       </div>
     );
     const classes = `dropdown${open ? ' is-active' : ''}`;
     return (
       <div className={classes}>
         <div className="dropdown-trigger">
-          <Button onClick={this.toggle}>{this.props.children}</Button>
+          <Button onClick={this.toggle} color={color}>
+            {children}
+          </Button>
         </div>
-        {content}
+        {menu}
       </div>
     );
   }
@@ -38,6 +42,11 @@ Dropdown.propTypes = {
     .isRequired,
   children: PropTypes.oneOfType([PropTypes.element, PropTypes.arrayOf(PropTypes.element)])
     .isRequired,
+  color: PropTypes.oneOf(COLOR),
+};
+
+Dropdown.defaultProps = {
+  color: '',
 };
 
 export default Dropdown;
