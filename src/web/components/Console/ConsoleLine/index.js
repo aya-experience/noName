fa444827bb30b8ebf99406f8113bd1717b576f85
Agory <gory.alexandre02@gmail.com>
2018-04-19 09:40:43 +0200

    feat(presentational): update style console

--- src/web/components/Console/ConsoleLine/index.js
+++ src/web/components/Console/ConsoleLine/index.js
@@ -5,10 +5,12 @@ import Level from '../../Level';
 import Icon from '../../Icon';
 import LevelItem from '../../LevelItem';
 import LevelArea from '../../LevelArea';
+import Tag from '../../Tag';
 
 const ConsoleLine = ({ data }) => {
   const style = {
     backgroundColor: data.type === 0 ? '#ff8989' : '#a1f28e',
+    marginBottom: '1px',
   };
   return (
     <li style={style}>
@@ -18,14 +20,14 @@ const ConsoleLine = ({ data }) => {
             <Icon name={data.type === 1 ? 'ion-social-javascript' : 'ion-iphone'} />
           </LevelItem>
           <LevelItem>
-            <Text>{JSON.stringify(data.args)}</Text>
+            <Text className="is-size-7">{JSON.stringify(data.args)}</Text>
           </LevelItem>
         </LevelArea>
         <LevelArea position="right">
           <LevelItem>
-            <Text>
+            <Tag className="is-radiusless" rounded color="info">
               {data.module}@{data.method}
-            </Text>
+            </Tag>
           </LevelItem>
         </LevelArea>
       </Level>
